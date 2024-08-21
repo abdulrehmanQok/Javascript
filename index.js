d58fc50c0f641d162d8ee3e3957dbd7ce12f1782
Abdul Rehman <116197769+abdulrehmanQok@users.noreply.github.com>
2024-08-21 05:56:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// var a=40;
+// var a=50;
+// console.log(a);
+// let b=10;
+// b=20;
+// console.log(b);
+
+// const c=30;
+// console.log(c);
+// let age =20;
+// if(age>=18){
+//     console.log('you are eligible to course');
+// }
+// else{
+//     console.log('you are not eligible to course');
+// }
+// let day= parseInt(prompt('enetr any value'));
+// if (day==1){
+//     alert('monday');
+// }
+// else if(day==2){
+//     alert('Today is tuesday');
+// }
+// else if(day==3){
+//     alert('Today is wednesday');
+// }
+// else if(day==4){
+//     alert('Today is thursday');
+// }
+// else if(day==5){
+//     alert('Today is friday');
+
+// }
+// else if(day==6){
+//     alert('Today is saturday');
+// }
+// else if(day==7){
+//     alert('Today is sunday');
+// }
+// else{
+//     alert('please enter a valid number between 1 and 7');
+// }
+// let a =20;
+// a>=30?(console.log("welcome to pakistan")):()
+for(var i=0; i<10;i++){
+    console.log("my anme is abdul rehman");
+
+}
+
+
